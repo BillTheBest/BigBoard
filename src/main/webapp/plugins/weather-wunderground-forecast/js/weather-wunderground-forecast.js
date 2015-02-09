2aa77c4ddeb581e297fce398e296a77d4ffaf7b0
Gordon Turner <gordon.turner@gmail.com>
2015-02-08 20:07:49 -0500

    Updates for Snow Showers and Light Freezing Rain.

--- src/main/webapp/plugins/weather-wunderground-forecast/js/weather-wunderground-forecast.js
+++ src/main/webapp/plugins/weather-wunderground-forecast/js/weather-wunderground-forecast.js
@@ -143,7 +143,7 @@ weatherWundergroundForecast.prototype.translateCondition = function( condition )
   
 
   if( condition == "Drifting Snow" || condition == "Light Snowshower" || condition == "Light Snow"
-      || condition == "Snowshower" || condition == "Chance of Snow" || condition == "Snow" ) {
+      || condition == "Snow Showers" || condition == "Chance of Snow" || condition == "Snow" ) {
     
     return this.pluginPath + "/images/snow-day.svg";
   }
@@ -157,6 +157,13 @@ weatherWundergroundForecast.prototype.translateCondition = function( condition )
 
   if( condition == "Freezing Rain" || condition == "Light Freezing Rain" ) {
     
+    return this.pluginPath + "/images/rain-freezing-day.svg";
+  }
+  
+
+  if( condition == "Light Freezing Rain" ) {
+    
+    condition = "Light Frz Rain"
     return this.pluginPath + "/images/rain-freezing-day.svg";
   }
   
